d0422aa7bf530c85bc03d57c03706d3886c5446e
Michel Jurzlins <j_linares@outlook.com>
2025-05-12 14:22:57 -0600

    update Ejecicio 3-2

--- web07-Js_Intro/ejercicios/myScript.js
+++ web07-Js_Intro/ejercicios/myScript.js
@@ -6,3 +6,18 @@ function saludar() {
 function cambiarColorFondo( color ) {
     document.body.style.backgroundColor = color; /* Color es variable */
 }
+
+function changeToGreenColor(){
+    const element = document.getElementById("green-paragraph")
+    console.log(element);
+    element.style.color = "green";
+}
+
+function changeColor(element, color){
+    element.style.color = color;
+}
+
+(function setUp(){
+    document.getElementById("title").style.color = "blue";
+    document.getElementById("title").style.fontSize = "50px";
+})();
