cc42a9b52b0ce6634a5270a15cce63a6f2349b3a
Michel Jurzlins <j_linares@outlook.com>
2025-05-12 13:38:17 -0600

    add Ejecicio 3 y 3-2

---
+++ web07-Js_Intro/ejercicios/myScript.js
@@ -0,0 +1,8 @@
+
+function saludar() {
+    alert("Ya casi nos vamos al segundo descanso");
+}
+
+function cambiarColorFondo( color ) {
+    document.body.style.backgroundColor = color; /* Color es variable */
+}
